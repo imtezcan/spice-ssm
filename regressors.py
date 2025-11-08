fix: speed up training, fix dt, try surrogate gradients
- save only model weights during training to speed up training
- use average kld as accuracy instead of the last one
- better cross-validation with validation set during training and a large test set during inference
- remove intermideate linear layer, replace exponential activation with softplus for more stability
- fix dt to min_dt and calculate max steps dynamically to enable more trials hitting the threshold
- add surrogate gradients to decision backward function to give non-zero grads to trials that did not cross the threshold (improve stability)

still constant drift for 0.5 or 0.1 do not train well.

regressors.py
@@ -169,31 +169,40 @@ class RNNRegressor(BaseEstimator):
                     plt.show()
 
                 # Cross validation with the validation set
-                self.rnn.eval()
-                with torch.no_grad():
-                    rts_fake, evidence = self.rnn.simulate(traces=False, n_sims=len(rt_val))
-                    rts_fake = rts_fake.squeeze().detach()
-                    kld = compute_kld(rts_fake, rt_val.squeeze().detach(), n_bins=50)
-                    self.logger.info(f'KL-divergence predicted || real: {kld} best: {best_kld}')
-                    accuracies_rnn.append(-kld)
+                last_kld = accuracies_rnn[-1] if accuracies_rnn else 0
+                if epoch % 10 == 0:
+                    self.logger.info(f'Evaluating RNN on validation set...')
+                    self.rnn.eval()
+                    with torch.no_grad():
+                        rts_fake, evidence = self.rnn.simulate(traces=False, n_sims=len(rt_val))
+                        rts_fake = rts_fake.squeeze().detach()
+                        kld = compute_kld(rts_fake, rt_val.squeeze().detach(), n_bins=100)
+                        # self.logger.info(f'KL-divergence predicted || real: {kld} best: {best_kld}')
+                        accuracies_rnn.append(-kld)
+                        acc_average = -np.mean(accuracies_rnn)
+                        self.logger.info(f'Average KL-divergence predicted || real: {acc_average} best: {best_kld}')
+                else:
+                    accuracies_rnn.append(last_kld)
+                min_delta = 0.0001
+                save_interval = 100
                 if early_stopping:
-                    if kld <= best_kld + 0.001:
-                        best_kld = kld
+                    if acc_average <= best_kld - min_delta:
+                        best_kld = acc_average
                         patience_counter = 0
-                        self.save_checkpoint()
+                        self.save_checkpoint_light()
                     else:
                         patience_counter += 1
                         if patience_counter > patience:
-                            self.logger.info(f'Early stopping at epoch {epoch + 1}, best kld: {best_kld}')
+                            self.logger.info(f'Early stopping at epoch {epoch + 1}, best accuracy: {best_kld}')
                             self.load_checkpoint(load_path=self.checkpoint_save_path)
                             break
-                else:
-                    self.save_checkpoint()
+                elif (epoch + 1) % save_interval == 0 or epoch + 1 == epochs:
+                    self.save_checkpoint_light()
         except KeyboardInterrupt:
             if not early_stopping:
                 self.logger.warning(
                     'KeyboardInterrupt detected. Aborting training, saving checkpoint and continuing with further operations.')
-                self.save_checkpoint()
+                self.save_checkpoint_full()
             else:
                 self.logger.warning(
                     'KeyboardInterrupt detected. Early stopping is on, continuing from checkpoint with best accuracy.')
@@ -244,10 +253,31 @@ class RNNRegressor(BaseEstimator):
         params = torch.load(load_path, map_location=self.device, weights_only=True)
         # load rnn and optim parameters from params dict
         self.rnn.load_state_dict(params['rnn'], strict=False)
-        # self.optim_rnn.load_state_dict(params['optimizer_rnn'])
+        if 'optimizer_rnn' in params:
+            self.optim_rnn.load_state_dict(params['optimizer_rnn'])
         if 'discriminator' in params:
             self.discriminator.load_state_dict(params['discriminator'])
+        if 'optimizer_discriminator' in params:
             self.optim_discriminator.load_state_dict(params['optimizer_discriminator'])
+
+    # inside RNNRegressor
+    def save_checkpoint_light(self):
+        checkpoint = {
+            'rnn': {k: v.cpu() for k, v in self.rnn.state_dict().items()},
+            'discriminator': {k: v.cpu() for k, v in self.discriminator.state_dict().items()},
+            'config': {'path_parameters': self.path_parameters},
+        }
+        torch.save(checkpoint, self.checkpoint_save_path, _use_new_zipfile_serialization=False)
+
+    def save_checkpoint_full(self):
+        checkpoint = {
+            'rnn': self.rnn.state_dict(),
+            'discriminator': self.discriminator.state_dict(),
+            'optimizer_rnn': self.optim_rnn.state_dict(),
+            'optimizer_discriminator': self.optim_discriminator.state_dict(),
+            'config': {'path_parameters': self.path_parameters},
+        }
+        torch.save(checkpoint, self.checkpoint_save_path, _use_new_zipfile_serialization=False)
 
     def save_checkpoint(self):
         # save model

rnn.py
@@ -41,12 +41,15 @@ class VectorizedEvidenceRNN(nn.Module):
         )
 
         # Drift & Diffusion layer
-        self.linear_dx = nn.Linear(hidden_dim, 2)
-        self.linear_output = nn.Linear(2, 2)
+        # self.linear_dx = nn.Linear(hidden_dim, 1)
+        # self.linear_output = nn.Linear(1, 1)
+
+        self.mu_head = nn.Linear(hidden_dim, 1)
 
         # Init weights and biases
-        nn.init.kaiming_uniform_(self.linear_dx.weight, nonlinearity='leaky_relu', a=0.2)
-        nn.init.xavier_uniform_(self.linear_output.weight)
+        # nn.init.kaiming_uniform_(self.linear_dx.weight, nonlinearity='leaky_relu', a=0.2)
+        # nn.init.xavier_uniform_(self.linear_output.weight)
+        nn.init.xavier_uniform_(self.mu_head.weight)
 
     def init_trial(self, init_evidence=None, batch_size=1):
         self.batch_size = batch_size
@@ -57,7 +60,8 @@ class VectorizedEvidenceRNN(nn.Module):
 
     def forward(self, h_input=None, traces=False, warmup=50):
         # max_steps = int(self.t_max / self.min_dt)
-        max_steps = 100
+        max_steps = int(torch.ceil(self.t_max / self.min_dt).item())
+        # max_steps = 100
         batch_size = self.batch_size
 
         hidden = torch.zeros(
@@ -92,19 +96,18 @@ class VectorizedEvidenceRNN(nn.Module):
 
         gru_out, _ = self.gru(h_input, hidden)
 
-        mu_sigma = self.linear_dx(gru_out)
-        mu_sigma = F.leaky_relu(mu_sigma, negative_slope=0.2)
-        mu_sigma = self.linear_output(mu_sigma)
-        mu_sigma = torch.exp(mu_sigma)
+        # mu = self.linear_dx(gru_out)
+        # mu = F.leaky_relu(mu, negative_slope=0.2)
+        # mu = self.linear_output(mu)
+        # mu = torch.exp(mu)
 
-        # Get drift rate
-        mu = mu_sigma[..., 0].unsqueeze(-1)
+        mu = F.softplus(self.mu_head(gru_out), beta=1.0)
 
-        # Get diffusion rate
-        sigma = mu_sigma[..., 1].unsqueeze(-1)
+        # Set sigma to 1.0 with the same shape as mu
+        sigma = torch.ones_like(mu, device=self.device)
 
         # Get timestep size
-        dt = self.t_max / max_steps
+        dt = torch.tensor(self.min_dt, device=self.device)
 
         # Calculate evidence
         epsilon = torch.randn((batch_size, max_steps, 1), device=self.device, requires_grad=False)

rnn_main.py
@@ -98,23 +98,18 @@ def main(output_dir, ddm_params, simulation_params, rnn_params, training_params,
         save_rt_data = False
         save_traces = False
     else:
-        rt, traces = simulate_rts(path_pybeam_model, phi, n_sims, logger)
+        rt_train, traces = simulate_rts(path_pybeam_model, phi, n_sims, logger)
+        rt_val, _ = simulate_rts(path_pybeam_model, phi, n_sims, logger)
 
     if save_rt_data:
-        save_rt_plot_and_data(rt, traces if save_traces else None, output_dir, logger)
+        save_rt_plot_and_data(rt_train, traces if save_traces else None, output_dir, logger)
 
-    # Split RT data into training and test set
-    t_max = np.max(abs(rt))
-    rt = torch.Tensor(rt).to(device)
-    rt_train = rt[:int(n_sims * train_test_ratio)]
-    if train_test_ratio == 1:
-        rt_test = rt_train
-    else:
-        rt_test = rt[int(n_sims * train_test_ratio):]
-
-    # Generate validation dataset
-    rt_val, _ = simulate_rts(path_pybeam_model, phi, 1024, logger)
-    rt_val = torch.Tensor(rt).to(device)
+    # Create training, validation and test sets
+    t_max_train = np.max(np.max(abs(rt_train)))
+    t_max_val = np.max(np.max(abs(rt_val)))
+    t_max = np.max([t_max_train, t_max_val])
+    rt_train = torch.Tensor(rt_train).to(device)
+    rt_val = torch.Tensor(rt_val).to(device)
 
     # Fit RNN
     rnn_regressor = RNNRegressor(hidden_dim=hidden_dim,
@@ -167,6 +162,9 @@ def main(output_dir, ddm_params, simulation_params, rnn_params, training_params,
     plt.close()
 
     # Simulate RTs and traces from the trained RNN
+    test_sims = int(np.floor(n_sims * train_test_ratio).item())
+    rt_test, _ = simulate_rts(path_pybeam_model, phi, test_sims, logger)
+    rt_test = torch.Tensor(rt_test).to(device)
     rt_rnn, traces_sim, dts_sim, decision_indices = rnn_regressor.predict(rt_test)
     mean_drift_rate = np.mean([traces_sim['drift'][i].mean() for i in range (len(traces_sim['drift']))])
     logger.info(f'Original drift rate: {drift_rate}, RNN mean drift rate: {mean_drift_rate}')

training_utils.py
@@ -44,7 +44,7 @@ def compute_kld(rt_real, rt_pred, n_bins=50):
     p = p + epsilon
     q = q + epsilon
 
-    return torch.sum(p * (torch.log(p) - torch.log(q))).detach().cpu()
+    return torch.sum(p * (torch.log(p) - torch.log(q))).detach().cpu().numpy()
 
 def fit_kdes(rt1, rt2, bandwidth='scott', kernel='gaussian'):
     kde1 = KernelDensity(bandwidth=bandwidth, kernel=kernel).fit(rt1.reshape(-1, 1))